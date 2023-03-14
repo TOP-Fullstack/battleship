2dabbc407634ecd8693b75df34b3f21057fb8769
Kevan <kevan.haggins@gmail.com>
2023-03-13 23:03:38 -0700

    Basic UI coming together. Issues with changing turns + users clicking the same square twice

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,4 +1,4 @@
-// import { render } from "./render.js";
+import { createDOM, allowClicks } from "./DOM.js";
 
 export const Ship = (length) => {
   let hits = 0;
@@ -9,7 +9,7 @@ export const Ship = (length) => {
   };
 
   const isSunk = () => {
-    if (hits == length) sunk = true;
+    if (hits === length) sunk = true;
     return sunk;
   };
 
@@ -18,106 +18,96 @@ export const Ship = (length) => {
 
 export const Gameboard = (size) => {
   const ships = [],
-    coords = [],
-    DOM = [];
+    coords = [];
 
   // Logic board
-  for (let y = 0; y < size; y++) {
+  for (let row = 0; row < size; row++) {
     const row = [];
-    for (let x = 0; x < size; x++) {
+    for (let column = 0; column < size; column++) {
       row.push("");
     }
     coords.push(row);
   }
 
-  const changeDOM = (y, x, piece) => {
-    if (coords[y][x] == "") piece.style.background = "black";
-    else if (coords[y][x] != "") piece.style.background = "red";
-  };
+  const DOM = createDOM(size);
 
-  // DOM board
-  const DOMboard = document.createElement("div");
-  DOMboard.className = "grid";
-  for (let y = 0; y < size; y++) {
-    const row = [];
-    for (let x = 0; x < size; x++) {
-      const DOMsquare = document.createElement("div");
-      DOMsquare.addEventListener("click", () => {
-        changeDOM(y, x, DOMsquare);
-      });
-      DOMboard.appendChild(DOMsquare);
-      row.push(DOMsquare);
-    }
-    DOM.push(row);
-  }
-  document.body.appendChild(DOMboard);
-
-  // Currently place it starting from the point clicked and iterate to the right by length
-  const place = (x, y, ship) => {
-    for (let i = y; i < y + ship.length; i++) {
-      coords[x][i] = ship;
+  // Adjust
+  const place = (column, row, ship) => {
+    for (let i = row; i < row + ship.length; i++) {
+      coords[column][i] = ship;
     }
     ships.push(ship);
   };
 
   const isGameOver = () => {
-    for (let ship of ships) {
-      if (!ship.isSunk()) return false;
-    }
+    ships.forEach((ship) => {
+      if (ship.isSunk() == false) return false;
+    });
     return true;
   };
 
-  const receivedAttack = (y, x) => {
-    if (typeof coords[y][x] == "object") {
-      coords[y][x].hit();
-      coords[y][x] = 1;
+  const receivedAttack = (row, column) => {
+    if (typeof coords[row][column] === "object") {
+      coords[row][column].hit();
+      coords[row][column] = 1;
+      DOM[row][column].style.background = "red";
     } else {
-      coords[y][x] = 0;
+      coords[row][column] = 0;
+      DOM[row][column].style.background = "black";
     }
-    DOM[y][x].click();
   };
 
-  return { coords, place, receivedAttack, isGameOver };
+  return { coords, place, receivedAttack, isGameOver, DOM };
 };
 
 export const Player = () => {
   const board = Gameboard(10);
   const attacked = [];
+  let turn = false;
 
-  // attack random piece on board only if it's coords[randomX+randomY] positions are "" or obj
+  const randomizeIndex = () => {
+    return Math.floor(Math.random() * board.coords.length);
+  };
+
+  const arrayIncludesArray = (arr, subarr) => {
+    return arr.some((elem) => elem.join(",") === subarr.join(","));
+  };
+
   const randomAttack = (gameboard) => {
-    let randomX, randomY, a, b, c;
+    let attack;
     do {
-      randomX = Math.floor(Math.random() * gameboard.coords[0].length);
-      randomY = Math.floor(Math.random() * gameboard.coords.length);
-      c = attacked.indexOf([randomY, randomX]);
-    } while (c == 1);
-    gameboard.receivedAttack(randomY, randomX);
+      attack = [randomizeIndex(), randomizeIndex()];
+    } while (arrayIncludesArray(attacked, attack));
+    attacked.push(attack);
+    gameboard.receivedAttack(attack[0], attack[1]);
   };
 
   // User attack
-  const attack = (y, x, gameboard) => {
-    gameboard.receivedAttack(y, x);
+  const attack = (row, column, gameboard) => {
+    gameboard.receivedAttack(row, column);
   };
 
-  return { attack, randomAttack, board };
+  return { attack, randomAttack, board, turn };
 };
 
 export const Game = () => {
+  let computer = false;
+
   // Initialize user's board
   const user = Player();
   const userShip1 = Ship(3);
-  const userShip2 = Ship(2);
-  user.board.place(0, 5, userShip1);
-  user.board.place(6, 1, userShip2);
+  user.board.place(0, 1, userShip1);
+  user.turn = true;
 
   // Initialize computer's board
-  const computer = Player();
-  const compShip1 = Ship(3);
-  const compShip2 = Ship(2);
-  computer.board.place(3, 2, compShip1);
-  computer.board.place(1, 4, compShip2);
+  const user2 = Player();
+  const user2Ship = Ship(3);
+  user2.board.place(0, 1, user2Ship);
 
-  // Allow user to click enemy board
-  return { user, computer };
+  // Allow the user to attack the enemy's board
+  allowClicks(user, user2);
+  allowClicks(user2, user);
+
+  // Check winner here
+  // no new methods in this object
 };
